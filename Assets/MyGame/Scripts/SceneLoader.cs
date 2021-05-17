24646fe42d1eaf1a75ed60a11d85ada270346d4f
LenaCupid <noah.schatzl01@htl-salzburg.ac.at>
2021-05-17 16:08:01 +0200

    Switch Scene Logic implemented

---
+++ Assets/MyGame/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	
+	public void funcScene1() {
+        SceneManager.LoadScene("Scene1");
+    }
+	
+	public void funcScene2() {
+        SceneManager.LoadScene("Scene2");
+    }
+}
